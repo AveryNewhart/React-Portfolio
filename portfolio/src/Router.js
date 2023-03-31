302d61fc4df8eb51d4cdc932f0d022b61b9a1189
AveryNewhart <newhartreeceavery@gmail.com>
2023-03-31 18:23:34 -0400

    all files added, packaged added

---
+++ portfolio/src/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from './pages/Contact'
+import Resume from "./pages/Resume";
+
+export default function Router() {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </>
+    );
+  }
