915008f7c95a842a541c4bde2648b3d2cd890aaa
AveryNewhart <newhartreeceavery@gmail.com>
2023-04-01 19:01:38 -0400

    mapped in front end and back end skills

---
+++ portfolio/src/pages/Resume.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
+import "../styles/App.css";
+import "../styles/Resume.css";
+import { UNSAFE_DataRouterStateContext } from 'react-router-dom';
+
+export default function Resume() {
+    //calling in useState and creating the array of objects that will be rendered on the page
+    const [frontEnd, setFrontEnd] = useState([
+        {
+            name: "HTML"
+        },
+        {
+            name: "CSS"
+        },
+        {
+            name: "JavaScript"
+        },
+        {
+            name: "Bootstrap"
+        },
+        {
+            name: "React"
+        },
+        {
+            name: "Jquery"
+        },
+        {
+            name: "Object-Oriented Programming - OOP"
+        },
+        {
+            name: "Responsive Design & Mobile-First"
+        },
+        {
+            name: "Debugging & Testing"
+        }
+    ]);
+
+    //calling in useState and creating the array of objects that will be rendered on the page
+    const [backEnd, useBackEnd] = useState([
+        {
+            name: "SQL Databases - SQL"
+        },
+        {
+            name: "NoSQL Databases - MongoDB"
+        },
+        {
+            name: "Express"
+        },
+        {
+            name: "Web Apis"
+        },
+        {
+            name: "Server Apis"
+        },
+        {
+            name: "Third-Party Apis"
+        },
+        {
+            name: "Object-Relational Mapping - ORM"
+        }
+    ]);
+
+    //put function here that will allow you to save my resume file
+
+  return (
+    <div className='main-div'>
+        <div className='main-head'>
+            <Header />
+        </div>
+        <div className='section-header'>
+            <h2 className='header-h2'>Resume</h2>
+        </div>
+        <div className='front-end-div'>
+            <div className='front-end-head'>
+                <h3 className='front-head'>Front End Skills</h3>
+            </div>
+            {frontEnd.map((skill) => {
+                return <li className='front-end-li'>{skill.name}</li>
+            })}
+        </div>
+        <div className='back-end-div'>
+            <div className='back-end-head'>
+                <h3 className='back-head'>Back End Skills</h3>
+            </div>
+            {backEnd.map((skill) => {
+                return <li className='back-end-li'>{skill.name}</li>
+            })}
+        </div>
+    </div>
+  )
+}
