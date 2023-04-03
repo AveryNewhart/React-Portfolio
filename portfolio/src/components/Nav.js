bbe005204ea5512152857369e4e83d0fb04e1876
AveryNewhart <newhartreeceavery@gmail.com>
2023-04-02 20:09:29 -0400

    all mobile responsive

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -2,7 +2,7 @@ import { React, useState } from "react";
 import "../styles/Nav.css";
 // functions being called in, location will be navigated to
 import { useLocation, useNavigate } from "react-router-dom";
-
+ 
 export default function Nav() {
     let location = useNavigate();
     const { pathname } = useLocation();
